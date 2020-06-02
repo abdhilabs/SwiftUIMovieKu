1fa5f2dc256dd3f94229abe9b125dacc6d28148b
Abdhilabs <rizaabdhi@gmail.com>
2020-06-03 00:14:41 +0700

    Initial Commit

---
+++ SwiftUIMovieKu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMovieKu
+//
+//  Created by Abdhi on 03/06/20.
+//  Copyright © 2020 Abdhilabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
